87c57c1ade490c2233b6a9f94ab941b367ec6290
Danny Hawk <hello@dannyhawk.com>
2020-07-20 14:02:37 -0400

    Version 1.0 commit

---
+++ CameraPositioner/CameraPositioner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class CameraPositioner : MonoBehaviour
+{
+    public void PositionCam()
+    {
+        this.transform.rotation = SceneView.lastActiveSceneView.rotation;
+        this.transform.position = SceneView.lastActiveSceneView.camera.transform.position;
+    }
+}
---
+++ CameraPositioner/Editor/CameraPositionerEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraPositioner))]
+public class CameraPositionerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        CameraPositioner camPos = (CameraPositioner)target;
+        if (GUILayout.Button("Position Camera"))
+        {
+            camPos.PositionCam();
+        }
+    }
+}
